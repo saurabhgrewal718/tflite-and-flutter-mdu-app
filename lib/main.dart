7499fe0bfef7d57f8d70eb9efbc397b8d7ae8fc6
Saurabh Grewal <saurabhgrewal718@gmail.com>
2021-07-14 18:34:07 +0530

    tflite is working and ui chanfges in the app

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:tflite/tflite.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:flutter/foundation.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 void main() {
   runApp(MyApp());
@@ -16,10 +18,214 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TfliteHome(),
+      theme: ThemeData(
+        // Define the default brightness and colors.
+        primaryColor: Colors.black,
+        accentColor: Colors.yellow),
+      home: OnBoardingPage(),
     );
   }
 }
+
+class OnBoardingPage extends StatefulWidget {
+  @override
+  _OnBoardingPageState createState() => _OnBoardingPageState();
+}
+
+class _OnBoardingPageState extends State<OnBoardingPage> {
+  final introKey = GlobalKey<IntroductionScreenState>();
+
+  void _onIntroEnd(context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(builder: (_) => TfliteHome()),
+    );
+  }
+
+  Widget _buildFullscrenImage() {
+    return Image.asset(
+      'assets/images/img1.gif',
+      fit: BoxFit.cover,
+      height: double.infinity,
+      width: double.infinity,
+      alignment: Alignment.center,
+    );
+  }
+
+  Widget _buildImage(String assetName, [double width = 350]) {
+    return Image.asset('assets/$assetName', width: width);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const bodyStyle = TextStyle(fontSize: 19.0);
+
+    const pageDecoration = const PageDecoration(
+      titleTextStyle: TextStyle(fontSize: 28.0, fontWeight: FontWeight.w700),
+      bodyTextStyle: bodyStyle,
+      descriptionPadding: EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+      pageColor: Colors.white,
+      imagePadding: EdgeInsets.zero,
+    );
+
+    return IntroductionScreen(
+      key: introKey,
+      globalBackgroundColor: Colors.white,
+      globalHeader: Align(
+        alignment: Alignment.topRight,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 16, right: 16),
+            child: _buildImage('images/img1.gif', 100),
+          ),
+        ),
+      ),
+      globalFooter: SizedBox(
+        width: double.infinity,
+        height: 60,
+        child: ElevatedButton(
+          child: const Text(
+            'Let\s go right away!',
+            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold,color: Colors.black),
+          ),
+          style: ElevatedButton.styleFrom(primary: Colors.yellow),
+          onPressed: () => _onIntroEnd(context),
+        ),
+      ),
+      pages: [
+        PageViewModel(
+          title: "Introduction",
+          body:
+              "Hello my name is saurabh and i made a object detector app for blinds",
+          image: _buildImage('images/img2.gif'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "How its made",
+          body:
+              "An AI model is trained on the Tenserflowlite and then this app uploads the images to that and then it predicts whats around you!",
+          image: _buildImage('images/img4.gif'),
+          decoration: pageDecoration,
+        ),
+        PageViewModel(
+          title: "How to use it",
+          body:
+              "just upload the image you want to check and then it tells you what you are watching and speaks to you",
+          image: _buildImage('images/img3.gif'),
+          decoration: pageDecoration,
+        ),
+        // PageViewModel(
+        //   title: "Fractional shares",
+        //   body:
+        //       "Instead of having to buy an entire share, invest any amount you want.",
+        //   image: _buildImage('images/img2.gif'),
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Fractional shares",
+        //   body:
+        //       "Instead of having to buy an entire share, invest any amount you want.",
+        //   image: _buildImage('images/img2.gif'),
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Learn as you go",
+        //   body:
+        //       "Download the Stockpile app and master the market with our mini-lesson.",
+        //   image: _buildImage('images/img1.gif'),
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Kids and teens",
+        //   body:
+        //       "Kids and teens can track their stocks 24/7 and place trades that you approve.",
+        //   image: _buildImage('img3.jpg'),
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Full Screen Page",
+        //   body:
+        //       "Pages can be full screen as well.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc id euismod lectus, non tempor felis. Nam rutrum rhoncus est ac venenatis.",
+        //   image: _buildFullscrenImage(),
+        //   decoration: pageDecoration.copyWith(
+        //     contentMargin: const EdgeInsets.symmetric(horizontal: 16),
+        //     fullScreen: true,
+        //     bodyFlex: 2,
+        //     imageFlex: 3,
+        //   ),
+        // ),
+        // PageViewModel(
+        //   title: "Another title page",
+        //   body: "Another beautiful body text for this example onboarding",
+        //   image: _buildImage('img2.jpg'),
+        //   footer: ElevatedButton(
+        //     onPressed: () {
+        //       introKey.currentState?.animateScroll(0);
+        //     },
+        //     child: const Text(
+        //       'FooButton',
+        //       style: TextStyle(color: Colors.white),
+        //     ),
+        //     style: ElevatedButton.styleFrom(
+        //       primary: Colors.lightBlue,
+        //       shape: RoundedRectangleBorder(
+        //         borderRadius: BorderRadius.circular(8.0),
+        //       ),
+        //     ),
+        //   ),
+        //   decoration: pageDecoration,
+        // ),
+        // PageViewModel(
+        //   title: "Title of last page - reversed",
+        //   bodyWidget: Row(
+        //     mainAxisAlignment: MainAxisAlignment.center,
+        //     children: const [
+        //       Text("Click on ", style: bodyStyle),
+        //       Icon(Icons.edit),
+        //       Text(" to edit a post", style: bodyStyle),
+        //     ],
+        //   ),
+        //   decoration: pageDecoration.copyWith(
+        //     bodyFlex: 2,
+        //     imageFlex: 4,
+        //     bodyAlignment: Alignment.bottomCenter,
+        //     imageAlignment: Alignment.topCenter,
+        //   ),
+        //   image: _buildImage('img1.jpg'),
+        //   reverse: true,
+        // ),
+      ],
+      onDone: () => _onIntroEnd(context),
+      //onSkip: () => _onIntroEnd(context), // You can override onSkip callback
+      showSkipButton: true,
+      skipFlex: 0,
+      nextFlex: 0,
+      //rtl: true, // Display as right-to-left
+      skip: const Text('Skip',style: TextStyle(color:Colors.black),),
+      next: const Icon(Icons.arrow_forward),
+      done: const Text('Done', style: TextStyle(color:Colors.black,fontWeight: FontWeight.w600)),
+      curve: Curves.fastLinearToSlowEaseIn,
+      controlsMargin: const EdgeInsets.all(16),
+      controlsPadding: kIsWeb
+          ? const EdgeInsets.all(12.0)
+          : const EdgeInsets.fromLTRB(8.0, 4.0, 8.0, 4.0),
+      dotsDecorator: const DotsDecorator(
+        size: Size(10.0, 10.0),
+        color: Color(0xFFBDBDBD),
+        activeSize: Size(22.0, 10.0),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+      ),
+      dotsContainerDecorator: const ShapeDecoration(
+        color: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(8.0)),
+        ),
+      ),
+    );
+  }
+}
+
 
 class TfliteHome extends StatefulWidget {
   @override
@@ -94,6 +300,7 @@ class _TfliteHomeState extends State<TfliteHome> {
 
     setState(() {
       _recognitions = recognitions;
+      print(_recognitions[0]["detectedClass"]);
     });
   }
 
@@ -169,3 +376,5 @@ class _TfliteHomeState extends State<TfliteHome> {
     );
   }
 }
+
+
